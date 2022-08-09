66d8028f19340e62864a7ebd8b982822a5e4fcbf
twtzptr <wildhare2020@yandex.ru>
2022-08-09 16:39:25 +0300

    dockerized

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { CitiesModule } from './cities/cities.module';
 import { CitizensModule } from './citizens/citizens.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     CitiesModule,
     CitizensModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/local'),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/local`,
+    ),
   ],
   controllers: [AppController],
   providers: [],
